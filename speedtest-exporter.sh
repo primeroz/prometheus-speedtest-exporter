7cd659380d6086edf7c2cae56bba361a5e8ba7a9
Francesco Ciocchetti <primeroznl@gmail.com>
2023-01-13 17:13:18 +0100

    Some updates

--- speedtest-exporter.sh
+++ speedtest-exporter.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
-# printMetric name description type value server_id
 function printMetric {
     echo "# HELP $1 $2"
     echo "# TYPE $1 $3"
-    if [ -z "$server_id" ]
-    then
-        echo "$1 $4"
-    else
-        echo "$1{server_id=\"$server_id\"} $4"
-    fi
+    echo "$1$5 $4"
 }
 
-if [ -z "$server_ids" ]
-then
-        while IFS=$'\t' read -r servername serverid latency jitter packetloss download upload downloadedbytes uploadedbytes share_url; do
-            printMetric "speedtest_latency_seconds" "Latency" "gauge" "$latency"
-            printMetric "speedtest_jittter_seconds" "Jitter" "gauge" "$jitter"
-            printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download"
-            printMetric "speedtest_upload_bytes" "Upload Speed" "gauge" "$upload"
-            printMetric "speedtest_downloadedbytes_bytes" "Downloaded Bytes" "gauge" "$downloadedbytes"
-            printMetric "speedtest_uploadedbytes_bytes" "Uploaded Bytes" "gauge" "$uploadedbytes"
-        done < <(/usr/local/bin/speedtest --accept-license --accept-gdpr -f tsv)
-else
-    IFS=',' read -ra server_id_array <<< "$server_ids"
-    for server_id in "${server_id_array[@]}"
-    do
-        while IFS=$'\t' read -r servername serverid latency jitter packetloss download upload downloadedbytes uploadedbytes share_url; do
-            printMetric "speedtest_latency_seconds" "Latency" "gauge" "$latency" "$server_id"
-            printMetric "speedtest_jittter_seconds" "Jitter" "gauge" "$jitter" "$server_id"
-            printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download" "$server_id"
-            printMetric "speedtest_upload_bytes" "Upload Speed" "gauge" "$upload" "$server_id"
-            printMetric "speedtest_downloadedbytes_bytes" "Downloaded Bytes" "gauge" "$downloadedbytes" "$server_id"
-            printMetric "speedtest_uploadedbytes_bytes" "Uploaded Bytes" "gauge" "$uploadedbytes" "$server_id"
-        done < <(/usr/local/bin/speedtest --accept-license --accept-gdpr -f tsv --server-id $server_id)
-    done
+speedtest_args=()
+speedtest_args+=("--accept-license")
+speedtest_args+=("--accept-gdpr")
+speedtest_args+=("--format=tsv")
+
+if [[ "x$1" != "x" ]];then
+  speedtest_args+=("--server_id=$1")
 fi
+
+while IFS=$'\t' read -r servername serverid latency jitter packetloss download upload downloadedbytes uploadedbytes rest; do
+    common_labels="{servername=\"$servername\",serverid=\"$serverid\"}"
+    printMetric "speedtest_latency_seconds" "Latency" "gauge" "$latency" "$common_labels"
+    printMetric "speedtest_jittter_seconds" "Jitter" "gauge" "$jitter" "$common_labels"
+    printMetric "speedtest_packet_loss" "Packet Loss" "gauge" "$packetloss" "$common_labels"
+    printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download" "$common_labels"
+    printMetric "speedtest_upload_bytes" "Upload Speed" "gauge" "$upload" "$common_labels"
+    printMetric "speedtest_downloadedbytes_bytes" "Downloaded Bytes" "gauge" "$downloadedbytes" "$common_labels"
+    printMetric "speedtest_uploadedbytes_bytes" "Uploaded Bytes" "gauge" "$uploadedbytes" "$common_labels"
+done < <(/usr/local/bin/speedtest "${speedtest_args[@]}")
